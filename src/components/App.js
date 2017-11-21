662bf6c3e388dd8af6b605b0475301553b800b56
Ross-Hunter <ross-hunter@ross-hunter.com>
2017-11-21 09:53:22 -0500

    adds an outline of the sign-in form

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
+import Login from './Login';
+import Profile from './Profile';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Login />
+        <Profile />
       </div>
     );
   }
---
+++ src/components/Login.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="Login">
+        <form onSubmit={this.handleSubmit}>
+          <h2>Sign In</h2>
+          <input type="text"
+                 name="username" />
+          <br />
+          <input type="password"
+                 name="password" />
+          <br />
+          <button type="submit">Sign In</button>
+        </form>
+      </div>
+    );
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  }
+}
+
+export default Login;
